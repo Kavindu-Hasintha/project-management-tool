5c6849db55fad689638aec6bd6395e9f4e2e1528
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2024-02-20 10:39:52 +0530

    Initial commit

---
+++ src/main/java/com/projectManagementTool/ProjectManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.projectManagementTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementToolApplication.class, args);
+	}
+
+}
